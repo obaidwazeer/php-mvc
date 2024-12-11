7dafb223250494f2d8914bd300240e726d64c809
obaidwazeer <obaidwazeer@gmail.com>
2024-12-11 13:20:50 -0800

    Implemented Routing successfully

---
+++ core/Router.php
@@ -0,0 +1,25 @@
+<?php 
+
+namespace App\core;
+
+class Router{
+    
+    public Request $request;
+    public function __construct(Request $request){
+        $this->request = $request;
+    }
+    protected $routes =[];
+    public function get($path, $callback){
+        $this->routes['get'][$path] = $callback;
+    }
+    public function resolve(){
+        $path = $this->request->getPath();
+        $method = $this->request->getMethod();
+        $callback = $this->routes[$method][$path] ?? false;
+        if($callback === false){
+            echo 'Not Found';
+            exit;
+        }
+        echo call_user_func($callback);
+    }
+}
